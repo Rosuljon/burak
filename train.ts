d908fe4dd13c17724371d22b10c7f2311b91ce1b
Rosuljon <ikramjanov97@mail.ru>
2025-03-11 02:06:13 +0900

    G-task added

--- train.ts
+++ train.ts
@@ -1,1 +1,16 @@
-console.log("train ts");
+//  ********************************   Task G     *****************************************
+
+function getHighestIndex(arr: number[]) {
+  let max: number = 0;
+  let index: number = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > max) {
+      max = arr[i];
+      index = i;
+    }
+  }
+  return index;
+}
+
+let arr: number[] = [5, 21, 12, 21, 8];
+console.log(getHighestIndex(arr));
