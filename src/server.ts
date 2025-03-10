c9853501085f1c3d07cc44974d85fb70b81fa84a
Rosuljon <ikramjanov97@mail.ru>
2025-03-11 04:31:33 +0900

    feat: connect to mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,13 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("successfully connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err);
+  });
