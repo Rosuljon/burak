979ee20634c7ce05f2d4f113a7d576cb161c59f8
Rosuljon <ikramjanov97@mail.ru>
2025-03-11 02:21:45 +0900

    fix: empty array and negative array

--- train.ts
+++ train.ts
@@ -1,7 +1,9 @@
 //  ********************************   Task G     *****************************************
 
-function getHighestIndex(arr: number[]) {
-  let max: number = 0;
+function getHighestIndex(arr: number[]): number {
+  if (arr.length === 0) return -1;
+
+  let max: number = arr[0];
   let index: number = 0;
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] > max) {
@@ -13,4 +15,8 @@ function getHighestIndex(arr: number[]) {
 }
 
 let arr: number[] = [5, 21, 12, 21, 8];
+let arr1: number[] = [];
+let arr2: number[] = [-7, -3, -9, -1];
 console.log(getHighestIndex(arr));
+console.log(getHighestIndex(arr1));
+console.log(getHighestIndex(arr2));
